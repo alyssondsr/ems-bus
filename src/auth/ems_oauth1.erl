10ceed9cd765ee36aa4ec0a721870372e9c0df25
alyssondsr <alysson-sr@hotmail.com>
2017-06-16 17:21:43 -0300

    cliente do Oauth 1.0a

--- src/auth/ems_oauth1.erl
+++ src/auth/ems_oauth1.erl
@@ -9,42 +9,26 @@
 		
 -import(proplists, [get_value/2]).
 
-serve_root(Request) ->
-  case lists:member(Request:get(method), ['GET', 'HEAD']) of
-    true ->
-      Request:respond({303, [{"Location", "/echo"}], <<>>});
-    false ->
-      method_not_allowed(Request)
-  end.
-
 serve_oauth_request_token(Request = #request{type = Type}) ->
   case Type of
     "GET" ->
-		ParamsUrl = Request#request.params_url,
-      	io:format("\n Request = ~s \n",[erlang:is_map(ParamsUrl)]),
-      	io:format("\n Request = ~s \n",[erlang:is_list(ParamsUrl)]),
-		{URL, Params, Consumer, Signature} = serve_oauth(Request), 
-      	io:format("\n URL = ~s \n",[URL]),
-		io:format("\n Signature = ~s \n",[Signature]),
-		URI = binary:bin_to_list(URL),
-		%io:format("\n oauth:verify(~s, ~s, ~s, ~s, ~s, ~s , ~s) \n",[Signature, Type, URL, Params, Consumer, <<>>]),
-        case oauth:verify(Signature, Type, URI, Params, Consumer, <<>>) of
+      serve_oauth(Request, fun(URL, Params, Consumer, Signature) ->
+        case oauth:verify(binary:bin_to_list(Signature), "GET", URL, maps:to_list(Params), Consumer, <<>>) of
           true ->
             ok(Request, <<"oauth_token=requestkey&oauth_token_secret=requestsecret">>);
           false ->
             bad(Request, "invalid signature value.")
-		end;
-      _ ->
+        end
+      end);
+    _ ->
       method_not_allowed(Request)
   end.
 
-serve_oauth_access_token(Request) ->
-io:fwrite("\n aqui \n"),
-
-  case Request:get(method) of
-    'GET' ->
-       {URL, Params, Consumer, Signature} = serve_oauth(Request),
-        case oauth:token(Params) of
+serve_oauth_access_token(Request = #request{type = Type}) ->
+  case Type of
+    "GET" ->
+      serve_oauth(Request, fun(URL, Params, Consumer, Signature) ->
+		case oauth:token(maps:to_list(Params)) of
           "requestkey" ->
             case oauth:verify(Signature, "GET", URL, Params, Consumer, "requestsecret") of
               true ->
@@ -54,15 +38,16 @@ io:fwrite("\n aqui \n"),
             end;
           _ ->
             bad(Request, "invalid oauth token.")
-        end;
+        end
+      end);
     _ ->
       method_not_allowed(Request)
   end.
 
-serve_echo(Request) ->
-  case Request:get(method) of
-    'GET' ->
-       {URL, Params, Consumer, Signature} = serve_oauth(Request),
+serve_echo(Request = #request{type = Type}) ->
+  case Type of
+    "GET" ->
+      serve_oauth(Request, fun(URL, Params, Consumer, Signature) ->
         case oauth:token(Params) of
           "accesskey" ->
             case oauth:verify(Signature, "GET", URL, Params, Consumer, "accesssecret") of
@@ -74,36 +59,36 @@ serve_echo(Request) ->
             end;
           _ ->
             bad(Request, "invalid oauth token")
-        end;
-       _ ->
+        end
+      end);
+    _ ->
       method_not_allowed(Request)
   end.
 
-serve_oauth(Request) ->
-	OauthVersion    = ems_request:get_querystring(<<"oauth_version">>, <<>>, Request),
-	case OauthVersion of
-		<<"1.0">> ->
-		  	ConsumerKey = ems_request:get_querystring(<<"oauth_consumer_key">>,<<>>, Request),
-			SigMethod = ems_request:get_querystring(<<"oauth_signature_method">>, <<>>, Request),
-			io:format("\n ConsumerKey = ~s \n",[ConsumerKey]),
-			io:format("\n SigMethod = ~s \n",[SigMethod]),
-			case consumer_lookup(ConsumerKey, SigMethod) of
-				none ->
-					bad(Request, "invalid consumer (key or signature method).");
-				Consumer ->
-					URL = ems_request:get_querystring(<<"oauth_callback">>, <<>>, Request),
-					Nonce = ems_request:get_querystring(<<"oauth_nonce">>, <<>>, Request),
-					Signature = ems_request:get_querystring(<<"oauth_signature">>, <<>>, Request),
-					Timestamp = ems_request:get_querystring(<<"oauth_timestamp">>, <<>>, Request),
-					%URL = string:concat("http://127.0.0.1:2301", Request:get(path)),
-					{URL,{OauthVersion, ConsumerKey, SigMethod, Nonce, Signature, Timestamp},Consumer, Signature}
-					%Fun(URL, proplists:delete("oauth_signature",Params), Consumer, Signature)
-				end;
-		_ ->
-			bad(Request, "invalid oauth version.")
+serve_oauth(Request, Fun) ->
+  Params = Request#request.querystring_map,
+  %io:format("\n map: ~s\n",[erlang:map_size(Params)]),
+  %io:format("\n map: ~s\n",[Params]),
+  case maps:get(<<"oauth_version">>, Params) of
+    <<"1.0">> ->
+      ConsumerKey = maps:get(<<"oauth_consumer_key">>, Params),
+      SigMethod = maps:get(<<"oauth_signature_method">>, Params),
+      case consumer_lookup(ConsumerKey, SigMethod) of
+        none ->
+          bad(Request, "invalid consumer (key or signature method).");
+        Consumer ->
+          io:format("\n aqui:  \n"),
+          Signature = maps:get(<<"oauth_signature">>, Params),
+          io:format("\n Signature: ~p \n",[Signature]),
+          URL = "http://127.0.0.1:2301", 
+          io:format("\n URL: ~p \n",[URL]),
+          Fun(URL, maps:remove(<<"oauth_signature">>, Params), Consumer, Signature)
+      end;
+    _ ->
+      bad(Request, "invalid oauth version.")
   end.
 
-consumer_lookup("key", "PLAINTEXT") ->
+consumer_lookup(<<"key">>, <<"PLAINTEXT">>) ->
   {"key", "secret", plaintext};
 consumer_lookup(<<"key">>, <<"HMAC-SHA1">>) ->
   {"key", "secret", hmac_sha1};
@@ -121,7 +106,6 @@ bad(Request, Reason) ->
   {ok, Request#request{code = 401, 
 								 response_data = list_to_binary("Bad Request: " ++ Reason)}
 	}.
-
 
 method_not_allowed(Request) ->
   Request:respond({405, [], <<>>}).
---
+++ src/auth/examples/ems_oauth1_client.erl
@@ -0,0 +1,73 @@
+-module(ems_oauth1_client).
+
+-export([callback/1]).
+-include("../include/ems_schema.hrl").
+-define(OAUTH_CALLBACK, <<"https://164.41.120.42:2302/callback">>).
+-define(CONSUMER_KEY, <<"key">>).
+-define(CONSUMER_SECRET, <<"123456">>).
+-define(OAUTH_SIGNATURE_METHOD, <<"PLAINTEXT">>).
+-define(ACCESS_TOKEN_URL, "https://164.41.120.43:2302/authorize").
+-define(SERVICO, <<"https://164.41.120.43:2302/netadm/info">>).
+
+%-define(REDIRECT_URI, <<"https://164.41.120.42:2302/callback">>).
+%-define(CLIENTID, <<"43138f88cb30a7b692f0">>).
+%-define(SECRET, <<"b45266981d747535974047853c2bb8ab5bba01bd">>).
+%-define(ACCESS_TOKEN_URL, "https://github.com/login/oauth/access_token").
+%-define(SERVICO, <<"https://api.github.com/user">>).
+
+
+callback(Request) -> 
+	Error = ems_request:get_querystring(<<"error">>, <<>>, Request),
+	case Error == <<"access_denied">> of
+		true ->
+			{ok, Request#request{code = 401, 
+				response_data = "{error: access_denied}"}
+			};
+		false -> 
+			Code = ems_request:get_querystring(<<"code">>, <<>>, Request),
+			Auth = base64:encode(<<?CLIENTID/binary, ":", ?SECRET/binary>>),
+			Authz = <<"Basic ", Auth/binary>>,
+			Authorization = binary:bin_to_list(Authz),
+			Databin =  <<"grant_type=authorization_code&code=", Code/binary, "&redirect_uri=", ?REDIRECT_URI/binary, "&scope=", ?SCOPE/binary>>,
+			Data = binary:bin_to_list(Databin),
+			case request(Authorization,Data) of
+				{ok,Response} ->
+					{ok, Request#request{code = 200, 
+								 response_data = Response,
+								 content_type = <<"application/json;charset=UTF-8">>}
+				};
+				{error,Error} ->
+					%ResponseData = ems_schema:to_json(Error),
+					{ok, Request#request{code = 401, 
+								 response_data = <<"{error:", Error/binary , "}">>,
+								 content_type = <<"application/json;charset=UTF-8">>}
+					}
+				
+			end			
+end.
+
+%funções internas
+
+acessa_servico(Token) ->
+	URLbin =  <<?SERVICO/binary, "?access_token=", Token/binary>>,
+	URL = binary:bin_to_list(URLbin),			
+	{ok, {{_Version, 200, _ReasonPhrase}, _Headers, Net}} = 
+		httpc:request(get,{URL, []}, [], []),
+	Net.	
+
+request(Authorization,Data)->
+	Response = httpc:request(post,{?ACCESS_TOKEN_URL, [{"Authorization", Authorization}], "application/x-www-form-urlencoded",Data}, [], []),		
+	format(Response).
+
+format({ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}})->
+	{ResponseData} = jiffy:decode(Body),
+	AccessToken = proplists:get_value(<<"access_token">>,ResponseData),
+    %RefreshToken = proplists:get_value(<<"reflesh_token">>, ResponseData),
+	{ok, acessa_servico(AccessToken)};
+	
+
+format(_Error)-> {error, <<"access_denied">>}.
+
+
+
+
